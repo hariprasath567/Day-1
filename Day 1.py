c3a4e42b05905c075093097e130badabf37dc5a5
hariprasath567 <aef940821@gmail.com>
2026-02-17 09:53:09 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,93 @@
+# print("hello world")
+
+# apple_Fruit=10
+# print(apple_Fruit)
+
+# Date type:
+# int,float,bool,str
+
+#  operators
+
+# birth_year=2006
+# current_year=2026
+# age=current_year-birth_year
+# print("your age",age)
+
+# current_age=20
+# max_age=25
+# estimated_amount=500
+# total=((max_age-current_age)*365)*estimated_amount
+# print(total)
+
+# current_age=20
+# max_age=25
+# liter_per_day=2
+# total_days=365
+# age_left=((max_age-current_age)*total_days)*liter_per_day
+# print(age_left)
+
+# trip_distance=1000
+# car_mileage=8
+# disel_price=92.69
+# liters_needed=trip_distance/car_mileage
+# total_expense=liters_needed*disel_price
+# print(liters_needed)
+# print(total_expense)
+
+# daily_data_usage=5
+# no_of_days=30
+# total=daily_data_usage*no_of_days
+# print(total)
+
+# current_age=20
+# years_into_days=current_age*365
+# print(years_into_days)
+
+# original_price=2000
+# discount_percentage=10
+# discount=discount_percentage/100*original_price
+# print(discount)
+# final_price=original_price-discount
+# print(final_price)
+
+# x=int(input("enter the value"))
+# y=int(input("enter the value"))
+# if(x==y):
+#     print("x and y are equal")
+# elif(x>y):
+#     print("x is geater than y")
+# else:
+#     print("x is less than y")
+
+# variable=input("enter the variables:")
+# if(variable==str("a"or"e"or"i"or"o"or"u")):
+#     print("its an vowel")
+# else:
+#     print("its an constant")
+
+# mark=int(input("enter your mark"))
+# if(mark==100):
+#     print("grade:s")
+# elif(mark>=90 and mark<=99):
+#     print("grade:a")
+# elif(mark>=80 and mark<=89):
+#     print("grade:b")
+# elif(mark>=70 and mark<=79):
+#     print("grade:c")
+# elif(mark>=60 and mark<=69):
+#     print("grade:d")
+# elif(mark>=50 and mark<=59):
+#     print("grade:e")
+# elif(mark>=40 and mark<=49):
+#     print("grade:f") 
+
+x=float(input("enter the price of dozen of banana:"))
+y=float(input("enter the price of single banana:"))
+value=y*12
+amt=value/x
+if(y<value):
+    print("loss:",amt)
+elif(y>value):
+    print("profit:",amt)
+elif(y==value):
+    print("there is no loss profit")
